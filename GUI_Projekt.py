8b543e249cad3e968cc763c6e3c6e1545015c525
Lorena Rodriguez <lorena.rodriguezfarina@stud.hslu.ch>
2022-11-21 15:54:57 +0100

    Titel des Fensters hinzugefügt sowie Fenstergrösse

--- GUI_Projekt.py
+++ GUI_Projekt.py
@@ -2,6 +2,8 @@ import PyQt5
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
 app = QApplication([])
 window = QWidget()
+window.setWindowTitle("Hypthekenrechner Gruppe Mix")
+window.setGeometry(400, 400, 280, 80)
 layout = QVBoxLayout()
 layout.addWidget(QPushButton('Top'))
 layout.addWidget(QPushButton('Bottom'))
