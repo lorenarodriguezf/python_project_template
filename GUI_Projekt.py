cbd7387e38fe29a4b2527d1e96ade22fda4e3b54
Ramona Weber <ramona.weber@stud.hslu.ch>
2022-11-28 14:50:47 +0100

    Funktion eingefügt

--- GUI_Projekt.py
+++ GUI_Projekt.py
@@ -12,6 +12,7 @@ window.setWindowTitle("Hypthekenrechner Gruppe Mix")
 window.setGeometry(600, 600, 480, 180)
 layout = QFormLayout()
 kaufpreis = QLineEdit()
+#on changed oder ähnliches für QlineEdit checken
 kaufpreis.setValidator (QIntValidator (1, 10000000, kaufpreis))
 einkommen = QLineEdit()
 einkommen.setValidator(QIntValidator (1, 20000000, einkommen))
@@ -22,6 +23,32 @@ layout.addRow("Jährliches Einkommen:", einkommen)
 layout.addRow("Eigenmittel:", eigenmittel)
 #Signals & Slots -> Pushbutton (In der Funktion definieren - Berechnung und Validierung / on button click verbinden mit Funktion)
 button = QPushButton ("Berechnen")
+#button.clicked.connect(function) --> Notwendig, um Funktion mit "Push the button" zu verbinden
+
+#def pushbutton ():
+    #if kaufpreis.text() == "" or einkommen.text() == "" or eigenmittel.text() == "":
+       # print ("Es sind nicht alle notwendigen Felder ausgefüllt")
+
+def pushbutton ():
+    zkaufpreis = kaufpreis.text()
+    zeinkommen = einkommen.text()
+    zeigenmittel = eigenmittel.text()
+    # neue Variable "z" definieren, da ansonsten ein Wert definiert wird, der noch nicht vorhanden ist --> kaufpreis.text greift den definierten Wert von oben auf, der neu in eine Zahl umgewandelt werden soll.
+    zkaufpreis = int(zkaufpreis)
+    zeinkommen = int(zeinkommen)
+    zeigenmittel = int(zeigenmittel)
+    Hypothek = zkaufpreis - zeigenmittel
+    print(Hypothek)
+    #Belehnung = (kaufpreis.text-eigenmittel.text) / kaufpreis.text
+    #print (Hypothek)
+    #print (Belehnung)
+
+    #zahlen
+    #zeinkommen = int(einkommen)
+    #zkaufpreis = int(kaufpreis)
+    #zeigenmittel = int(eigenmittel)
+button.clicked.connect (pushbutton)
+Hypothek = QLabel("Hypothek")
 layout.addWidget(button)
 window.setLayout(layout)
 window.setLayout(layout)
