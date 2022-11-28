cb5a848bd0f00ef786d52654fec3cc4399cb6ef2
Ramona Weber <ramona.weber@stud.hslu.ch>
2022-11-28 11:20:41 +0100

    Style und Farbe eingefügt

--- GUI_Projekt.py
+++ GUI_Projekt.py
@@ -1,6 +1,12 @@
 import PyQt5
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
 app = QApplication([])
+app.setStyle('Windows')
+palette = QPalette()
+palette.setColor(QPalette.ButtonText, Qt.red)
+app.setPalette(palette)
 window = QWidget()
 window.setWindowTitle("Hypthekenrechner Gruppe Mix")
 window.setGeometry(400, 400, 280, 80)
