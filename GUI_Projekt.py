0acaca1acc246e50583a85928143566b90846905
Matej Maric <matej.maric@ost.ch>
2022-11-28 11:59:54 +0100

    Layoutanpassung

--- GUI_Projekt.py
+++ GUI_Projekt.py
@@ -1,7 +1,7 @@
 import PyQt5
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QFormLayout, QLineEdit
 app = QApplication([])
 app.setStyle('Windows')
 palette = QPalette()
@@ -9,11 +9,14 @@ palette.setColor(QPalette.ButtonText, Qt.red)
 app.setPalette(palette)
 window = QWidget()
 window.setWindowTitle("Hypthekenrechner Gruppe Mix")
-window.setGeometry(400, 400, 280, 80)
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Kaufpreis'))
-layout.addWidget(QPushButton('Jährliches Bruttoeinkommen'))
-layout.addWidget(QPushButton('Eigenmittel'))
+window.setGeometry(600, 600, 480, 180)
+layout = QFormLayout()
+layout.addRow("Kaufpreis:", QLineEdit())
+layout.addRow("Jährliches Einkommen:", QLineEdit())
+layout.addRow("Eigenmittel:", QLineEdit())
+#Signals & Slots -> Pushbutton (In der Funktion definieren - Berechnung und Validierung)
+layout.addWidget(QPushButton('Berechnung'))
+window.setLayout(layout)
 window.setLayout(layout)
 window.show()
 app.exec()
